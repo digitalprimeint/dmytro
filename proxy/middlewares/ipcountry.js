82910827dbf0596ba4a41bc6cf70fb8aa6c0db8c
Marco Castillo <marco@plugdo.com>
2022-06-10 15:25:55 -0500

    added remote ip header

--- proxy/middlewares/ipcountry.js
+++ proxy/middlewares/ipcountry.js
@@ -1,15 +1,12 @@
 const geoip = require('geoip-country');
-const ip = require('ip');
 
 module.exports = (req, res, next) => {
-    let ip1 = ip.address();
-    let ip2 = req.headers["X-Forwarded-For"];
-    let ip3 = req.headers['X-Appengine-User-Ip'];
+    let ip1 = req.headers["x-forwarded-for"];
     let ip_active = process.env.IP_ACTIVE === "REMOTE" ? ip1 : process.env[process.env.IP_ACTIVE];
 
     let lookup = geoip.lookup(ip1);
     lookup = lookup === null ? geoip.lookup(ip_active) : lookup;
-    console.log("country", lookup === null ? "NOT FOUND" : lookup.country, "ips", ip1, ip2, ip3);
+    console.log("country", lookup === null ? "NOT FOUND" : lookup.country, "ips", ip1);
     console.log(req.headers);
 
     if(lookup !== null) {
